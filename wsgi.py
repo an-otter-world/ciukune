c1ec3a5d79b952d999dfa57b9e5e5e69569356b8
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-11-26 09:45:44 +0100

    Moved TODO, cleaned some files headers & documentation

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 # Copyright © 2019 STJV <contact@stjv.fr>
 #
-# This work is free. You can redistribute it and/or modify it under the terms
-# of the Do What The Fuck You Want To Public License, Version 2, as published
-# the comrade Sam Hocevar.
+# This work is free. You can redistribute it and/or modify it under the terms of
+# the Do What The Fuck You Want To Public License, Version 2, as published by
+# Sam Hocevar.
 #
 # See the COPYING file for more details.
 """
@@ -13,7 +13,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
