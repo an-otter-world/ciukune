aa5f323d5efd4dad7ee4f22b4396808a2c0625e4
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-11-28 15:28:20 +0100

    Fixed setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-echo "Setting up python virtual environment"
-pip3 install virtualenv
+echo "Setting up python environment"
+pip3 install virtualenv > /dev/null
+
+if [ -d .env ]; then
+	rm -fr .env
+fi
+
+virtualenv .env
 source .env/bin/activate
+
+echo "Installing python packages"
 pip install -r requirements.txt > /dev/null
 
 echo "Setting up nodeenv..."
 pip install nodeenv > /dev/null
+nodeenv -p
 
 echo "Installing node modules"
 pushd kileed/ui > /dev/null
