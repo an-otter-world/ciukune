3d1ea93bb1170f1c82546a8ff1d57609e5ebbaab
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-12-01 22:22:11 +0100

    Installing firefox for e2e tests step

--- ci/e2e-tests.sh
+++ ci/e2e-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env/bin/activate
 
-apt install -y libgtk-3-0
+apt install -y firefox
 
 python manage.py test kileed.tests.e2e
 cat geckodriver.log
--- ci/setup-workspace.sh
+++ ci/setup-workspace.sh
@@ -23,12 +23,6 @@ npm install
 popd > /dev/null
 
 echo "Setting up Splinter test environment..."
-mkdir -p .env/opt > /dev/null
-pushd .env/opt
-wget -qO- 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US' | tar xj
-popd
-ln -s $PWD/.env/opt/firefox/firefox .env/bin/firefox
-
 pushd .env/bin
 wget -qO- https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz\
 	| tar xz
