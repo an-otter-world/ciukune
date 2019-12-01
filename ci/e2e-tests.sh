87b375f3158900a8ac6f58b1d01c4c33b934b67c
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-12-01 22:27:04 +0100

    updating packages before launching e2e tests

--- ci/e2e-tests.sh
+++ ci/e2e-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source .env/bin/activate
 
+echo "deb http://fr.archive.ubuntu.com/ubuntu/ bionic main restricted" > /etc/apt/sources.list
+ap-get update
 apt install -y firefox
 
 python manage.py test kileed.tests.e2e
