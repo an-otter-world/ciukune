cdd78525a5c98c23ebd77d7df12abced3a732b8b
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-12-01 22:29:44 +0100

    Fixed typo

--- ci/e2e-tests.sh
+++ ci/e2e-tests.sh
@@ -2,7 +2,7 @@
 source .env/bin/activate
 
 echo "deb http://fr.archive.ubuntu.com/ubuntu/ bionic main restricted" > /etc/apt/sources.list
-ap-get update
+apt update
 apt install -y firefox
 
 python manage.py test kileed.tests.e2e
