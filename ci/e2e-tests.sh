123ac48737e91d58721e92f6bc287e75e10419ee
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-12-01 23:42:30 +0100

    Added some ubuntu repo when trying to run e2e tests

--- ci/e2e-tests.sh
+++ ci/e2e-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source .env/bin/activate
-
 add-apt-repository main
+add-apt-repository 'deb http://fr.archive.ubuntu.com/ubuntu/ bionic-updates main restricted'
 apt update
 apt install -y firefox firefox-gecko-driver python3
 
+source .env/bin/activate
 python manage.py test kileed.tests.e2e
 cat geckodriver.log
