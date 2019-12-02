979536f1740b396daf1eb19d7482c6e0fa6ad872
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-12-02 14:55:28 +0100

    Fixed npm installation & infinite recursion for pylint & pydocstyle

--- scripts/drone.sh
+++ scripts/drone.sh
@@ -1,64 +1,38 @@
 #!/bin/bash
 
-
-api-unit-tests() {
-    source .env/bin/activate
+api-unit-tests-command() {
     python manage.py test kileed.tests.unit
 }
 
-build-ui() {
-    source .env/bin/activate
+build-ui-command() {
     pushd kileed/ui > /dev/null
         npm run build
     popd > /dev/null
 }
 
-e2e-tests() {
-    source .env/bin/activate
+e2e-tests-command() {
     python manage.py test kileed.tests.e2e
 }
 
-eslint() {
-    source .env/bin/activate
+eslint-command() {
     pushd kileed/ui > /dev/null
         ./node_modules/.bin/eslint src/**/*.{vue,js}
     popd > /dev/null
 }
 
-pydocstyle() {
-    source .env/bin/activate
+pydocstyle-command() {
     pydocstyle
 }
 
-pylint() {
-    source .env/bin/activate
+pylint-command() {
     pylint kileed
 }
 
-setup-workspace() {
-    echo "Setting up python environment"
-    pip3 install virtualenv > /dev/null
-
-    virtualenv -p python3 .env
-    source .env/bin/activate
-
-    echo "Installing python packages"
-    pip install -r requirements-dev.txt > /dev/null
-
-    echo "Setting up nodeenv..."
-    nodeenv -p
-
-    echo "Installing node modules"
-    pushd kileed/ui > /dev/null
-    npm install
-    popd > /dev/null
-}
-
-ui-unit-tests() {
+ui-unit-tests-command() {
     source .env/bin/activate
     pushd kileed/ui > /dev/null
         npm run test:unit
     popd > /dev/null
 }
 
-$1
+$1-command
