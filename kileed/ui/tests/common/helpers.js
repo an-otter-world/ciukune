6a5bdba46fc5dcbde7fc3667de522f0e6bbff061
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2019-12-05 13:05:20 +0100

    Fixed unit tests

--- kileed/ui/tests/common/helpers.js
+++ kileed/ui/tests/common/helpers.js
@@ -3,6 +3,16 @@ import Vuetify from 'vuetify/lib'
 import Vuex from 'vuex'
 import { mount as baseMount } from '@vue/test-utils'
 
+// Patches because vue unit tests sucks
+global.requestAnimationFrame = (cb) => cb()
+
+global.window = Object.create(window)
+Object.defineProperty(window, 'location', {
+  value: {
+    href: undefined
+  }
+})
+
 /* Using global vue instance instead of localVue because of
  * https://github.com/vuetifyjs/vuetify/issues/4068. We should switch back
  * to localVue when this is fixed.
