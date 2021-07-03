302f87c30c415a2385f878ac42a1c28367c2fe0b
an-otter-world <59399648+an-otter-world@users.noreply.github.com>
2021-07-04 00:30:27 +0200

    Started profile form.

--- ciukune-ui/src/api/types/user.ts
+++ ciukune-ui/src/api/types/user.ts
@@ -1,3 +1,4 @@
 export interface User {
-  email: string
+  email: string,
+  username: string
 }
--- ciukune-ui/src/views/profile.vue
+++ ciukune-ui/src/views/profile.vue
@@ -1,3 +1,28 @@
 <template lang="pug">
-h1 Profile
+ciu-component
+  header
+    h1 Profile
+  ciu-api-form(:resource="me")
+    ciu-api-errors
+    ciu-api-input(field="email")
+      ciu-text-field(:placeholder="$t('profile.email')" v-model="email")
+    ciu-api-input(field="username")
+      ciu-text-field(:placeholder="$t('profile.username')" v-model="username")
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { getMeResource } from '../api/resources/auth/me'
+
+export default defineComponent({
+    setup() {
+      const me = getMeResource()
+
+      return {
+	me,
+        email: me.state.email,
+	username: me.state.username,
+      }
+    },
+})
+</script>
